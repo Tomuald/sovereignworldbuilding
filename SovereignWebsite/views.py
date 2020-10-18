2c0b809908e4536189b2b3e8933c02293a3216bf
thomas <thomas.garriss@gmail.com>
2020-10-17 20:32:23 -0400

    Dungeon room 'connected_to' fix

--- SovereignWebsite/views.py
+++ SovereignWebsite/views.py
@@ -931,7 +931,7 @@ def room_create(request, in_roomset, pk=None):
 		room = Room()
 	
 	rooms = Room.objects.filter(in_roomset=in_roomset.id).exclude(id=room.id)
-	connected_to = Room.objects.filter(in_roomset=room.in_roomset.id).filter(exits=room.id)
+	connected_to = Room.objects.filter(in_roomset.id=room.in_roomset.id).filter(exits=room.id)
 	
 	form = RoomModelForm(rooms,
 						 roomsets,
