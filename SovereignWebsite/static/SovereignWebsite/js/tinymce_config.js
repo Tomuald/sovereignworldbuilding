bfe55ea4a0abfe8df4f3cabab0c066cb9668dab4
thomas <thomas.garriss@gmail.com>
2020-12-03 21:35:09 -0500

    Added lists to tinymce.

--- SovereignWebsite/static/SovereignWebsite/js/tinymce_config.js
+++ SovereignWebsite/static/SovereignWebsite/js/tinymce_config.js
@@ -4,7 +4,7 @@ tinymce.init({
         plugins: 'link lists table hr toc',
         toc_depth: 6,
         toc_header: 'h3',
-        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link | table | hr toc ',
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link | numlist bullist table | hr toc ',
         menubar: 'edit view insert format',
         relative_urls: false,
         
